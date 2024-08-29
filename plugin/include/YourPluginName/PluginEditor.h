664b68ef0bd137bc854aa2c2d25a1be821adf2b1
Keatsm <keatsm@zoho.com>
2024-08-30 03:17:37 +1000

    started development

--- plugin/include/YourPluginName/PluginEditor.h
+++ plugin/include/YourPluginName/PluginEditor.h
@@ -9,13 +9,15 @@ public:
   explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor&);
   ~AudioPluginAudioProcessorEditor() override;
 
-  void paint(juce::Graphics&) override;
-  void resized() override;
-
 private:
   // This reference is provided as a quick way for your editor to
   // access the processor object that created it.
   AudioPluginAudioProcessor& processorRef;
+
+  juce::WebBrowserComponent webBrowserComponent;
+  juce::TextButton downloadButton { "Import Video" };
+
+  auto downloadAudio() -> void;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
--- plugin/source/PluginEditor.cpp
+++ plugin/source/PluginEditor.cpp
@@ -6,27 +6,21 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(
     AudioPluginAudioProcessor& p)
     : AudioProcessorEditor(&p), processorRef(p) {
   juce::ignoreUnused(processorRef);
-  // Make sure that before the constructor has finished, you've set the
-  // editor's size to whatever you need it to be.
-  setSize(400, 300);
+  addAndMakeVisible(webBrowserComponent);
+  webBrowserComponent.goToURL("https://www.youtube.com/");
+  addAndMakeVisible(downloadButton);
+  
+  downloadButton.onClick = [this] { this->downloadAudio(); };
+  
+  setSize (600, 400);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor() {}
 
-void AudioPluginAudioProcessorEditor::paint(juce::Graphics& g) {
-  // (Our component is opaque, so we must completely fill the background with a
-  // solid colour)
-  g.fillAll(
-      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
-
-  g.setColour(juce::Colours::white);
-  g.setFont(15.0f);
-  g.drawFittedText("Hello World!", getLocalBounds(),
-                   juce::Justification::centred, 1);
-}
-
-void AudioPluginAudioProcessorEditor::resized() {
-  // This is generally where you'll want to lay out the positions of any
-  // subcomponents in your editor..
+auto AudioPluginAudioProcessorEditor::downloadAudio() -> void {
+  webBrowserComponent.evaluateJavascript("window.location.href", [this](const juce::WebBrowserComponent::EvaluationResult& result)
+  {
+    auto url = result.getResult()->toString();
+  });
 }
 }  // namespace audio_plugin
